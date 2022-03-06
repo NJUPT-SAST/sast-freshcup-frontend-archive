907a1872b6a8466e128c29886de8fd951c76e801
MaxtuneLee <60775796+MaxtuneLee@users.noreply.github.com>
2022-03-06 22:49:52 +0800

    feat: 比赛设置、选手账号、管理员授权界面

--- src/pages/SuperAdmin/CompetitionManage/QuestionSetting/QuestionSetting.tsx
+++ src/pages/SuperAdmin/CompetitionManage/QuestionSetting/QuestionSetting.tsx
@@ -1,3 +1,15 @@
+import { Typography } from '@douyinfe/semi-ui';
+import './QuestionSetting.sass'
+
+const {Title} = Typography
+
 export default function QuestionSetting() {
-  return <>QuestionSetting</>;
+  return (
+    <>
+      <div className='inputGroup'>
+      <Title heading={6} className="title">题目设置</Title>
+      <div>拖拽列表</div>
+      </div>
+    </>
+    );
 }
