821424474a1a7b6c674167b3dfffe70d66220d97
msuad <1445617868@qq.com>
2022-05-04 21:47:44 +0800

    feat: admin api函数的编写，并加了点注释

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//===Start||get Method===
 export const getCompetitionList = (
   pageNum: string,
   pageSize: string,
@@ -38,6 +39,25 @@ export const getIssuesList = (
   });
 };
 
+export const getIssueDetailedInfo=( id:string , TOKEN:any )=>{
+  return axios.get("/admin/problemInfo", {
+    //  Query Params
+    params: {
+      id
+    },
+    // Header Params
+    headers: {
+      TOKEN,
+    },
+  });
+}
+//===End||get Method===
+
+//===Start||Post Method===
+
+/**
+ * @brief 删除题目
+ */
 export const deleteIssue = (id: string | undefined, TOKEN: any) => {
   return axios.post(
     "/admin/deleteProblem",
@@ -55,6 +75,9 @@ export const deleteIssue = (id: string | undefined, TOKEN: any) => {
   );
 };
 
+/**
+ * @brief 批改题目
+ */
 export const correctIssue = (
   answerId: number,
   judgerId: number,
@@ -78,3 +101,95 @@ export const correctIssue = (
     }
   );
 };
+
+/**
+ * @brief 出题
+ */
+export const setIssue = (
+    contestId: number,
+    ownerUid: number,
+    type: number,
+    answer:string,
+    desc:string,
+    opt1:string,
+    opt2:string,
+    opt3:string,
+    opt4:string,
+    score:number,
+    order_id:number,
+    pic:string,
+    TOKEN: any
+) => {
+  return axios.post(
+      "/admin/setProblem",
+      {
+          contestId,
+          ownerUid,
+          type,
+          answer,
+          desc,
+          opt1,
+          opt2,
+          opt3,
+          opt4,
+          score,
+          order_id,
+          pic
+      },
+      {
+        //  Query Params
+        params: {},
+        // Header Params
+        headers: {
+          TOKEN,
+        },
+      }
+  );
+};
+
+/**
+ * @brief 修改题目内容
+ */
+export const updateIssue = (
+    contestId: number,
+    ownerUid: number,
+    type: number,
+    answer:string,
+    desc:string,
+    opt1:string,
+    opt2:string,
+    opt3:string,
+    opt4:string,
+    score:number,
+    order:number,
+    pic:string,
+    id:number,
+    TOKEN: any
+) => {
+    return axios.post(
+        "/admin/updateProblem",
+        {
+            contestId,
+            ownerUid,
+            type,
+            answer,
+            desc,
+            opt1,
+            opt2,
+            opt3,
+            opt4,
+            score,
+            order,
+            pic
+        },
+        {
+            //  Query Params
+            params: {},
+            // Header Params
+            headers: {
+                TOKEN,
+            },
+        }
+    );
+};
+//===End||Post Method===
